773685b9c7ebcd06d76a73b5a97ffb6db03df6e4
satwikkansal <satwikkansal@gmail.com>
2017-02-13 17:21:40 +0530

    Defining colors and adding background

--- first_game.py
+++ first_game.py
@@ -3,18 +3,25 @@ import pygame
 #initailize
 pygame.init()
 
+#Defining colors (rgb values)
+BACKGROUND_COLOR = (178, 217, 4)
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+
+
 #set up the display
 gameDisplay = pygame.display.set_mode((800,600))
 pygame.display.set_caption("PyPyper")
-
-#update the display
-pygame.display.update() 
 
 gameExit = False
 while not gameExit:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			gameExit = True
+
+	gameDisplay.fill(BACKGROUND_COLOR)
+	pygame.display.update()
 
 
 #exit
