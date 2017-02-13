5ddffec33ffc46f59d37a7357e54cfbf08cfbb84
satwikkansal <satwikkansal@gmail.com>
2017-02-13 17:00:08 +0530

    Create basic structure

---
+++ first_game.py
@@ -0,0 +1,14 @@
+import pygame
+
+#initailize
+pygame.init()
+
+#set up the display
+gameDisplay = pygame.display.set_mode((800,600))
+
+#update the display
+pygame.display.update()
+
+#exit
+pygame.quit()
+quit()
