158526528275c72d27624c7e7f49aa9033738e7a
satwikkansal <satwikkansal@gmail.com>
2017-02-13 17:04:10 +0530

    Looping through the events

--- first_game.py
+++ first_game.py
@@ -5,9 +5,16 @@ pygame.init()
 
 #set up the display
 gameDisplay = pygame.display.set_mode((800,600))
+pygame.display.set_caption("PyPyper")
 
 #update the display
-pygame.display.update()
+pygame.display.update() 
+
+gameExit = False
+while not gameExit:
+	for event in pygame.event.get():
+		print(event)
+
 
 #exit
 pygame.quit()
